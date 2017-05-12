a0be5f19eed9f63d9175113a5ca5bc615f7dcbca
David Green <pbcub1.dg@gmail.com>
2017-05-12 15:05:47 -0600

    Outline
    Developed outline the basic HTML structure that I will use in this
    project.

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<!--Items that will need to be edited to match branding-->
+		<title>Sound Application</title>
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<meta name="keywords" content="">
+
+		<!--Styles-->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="res/styles/main.css">
+	</head>
+	<body>
+		<!--Main navigational navbar-->
+		<div class="navbar navbar-default">
+
+		</div>
+
+		<!--Body-->
+		<div class="container">
+
+		</div>
+
+		<!--Footer navbar-->
+		<div class="navbar navbar-default">
+
+		</div>
+
+		<!--Scripts-->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="src/cli/app.js"></script>
+	</body>
+</html>
