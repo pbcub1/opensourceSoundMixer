9ba5aede24f2c125acd261c556f3a0c3652a9ef1
David Green <pbcub1.dg@gmail.com>
2017-05-13 15:09:55 -0600

    Dependencies
    Had to collect a few libraries to program off of. I have Wad.js which
    will do the DAW, I have Recorder.js which will record and then download
    the file, I have Bootstrap for CSS templating, I have jQuery because
    glue is a must, and I have a basic script for file uploading that I
    pulled from the internet. All of these will create the app as needed.

--- src/cli/app.js
+++ src/cli/app.js
@@ -28,3 +28,50 @@ $(document).ready(function(){
 		}
 	});
 });
+
+//Simple File Uploader from http://stackoverflow.com/questions/166221/how-can-i-upload-files-asynchronously
+//TODO: Change Size
+//TODO: Check type to make sure it's in proper format
+$(':file').on('change', function() {
+    var file = this.files[0];
+    if (file.size > 1024) {
+        alert('max upload size is 1k')
+    }
+
+    // Also see .name, .type
+});
+
+//TODO: Check Ajax return request to save URL for audio processing.
+$(':button').on('click', function() {
+    $.ajax({
+        // Your server script to process the upload
+        url: 'upload.php',
+        type: 'POST',
+
+        // Form data
+        data: new FormData($('form')[0]),
+
+        // Tell jQuery not to process data or worry about content-type
+        // You *must* include these options!
+        cache: false,
+        contentType: false,
+        processData: false,
+
+        // Custom XMLHttpRequest
+        xhr: function() {
+            var myXhr = $.ajaxSettings.xhr();
+            if (myXhr.upload) {
+                // For handling the progress of the upload
+                myXhr.upload.addEventListener('progress', function(e) {
+                    if (e.lengthComputable) {
+                        $('progress').attr({
+                            value: e.loaded,
+                            max: e.total,
+                        });
+                    }
+                } , false);
+            }
+            return myXhr;
+        },
+    });
+});
